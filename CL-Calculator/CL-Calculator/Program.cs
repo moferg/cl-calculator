de564823b16c41ac35a84135822342416ca7066e
Marshall Ferguson <moferg01@louisville.edu>
2018-04-18 08:25:10 -0400

    Added pseudocode outline

--- CL-Calculator/CL-Calculator/Program.cs
+++ CL-Calculator/CL-Calculator/Program.cs
@@ -10,6 +10,15 @@ namespace CL_Calculator
     {
         static void Main(string[] args)
         {
+            // while user does not press 'q'...
+                // ...display menu and accept user input
+                // if user input is 1 call addition function
+                // if user input is 2 call subtraction function
+                // if user input is 3 call multiplication function
+                // if user input is 4 call division function
+                // if user input is q exit program
+
+            // Note to self: do I need seperate functions to call or just use the built in math functions?
         }
     }
 }
