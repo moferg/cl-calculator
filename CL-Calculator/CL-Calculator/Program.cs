2bdc74567749fc9af624f3342062964d5ac1503e
Marshall Ferguson <moferg01@louisville.edu>
2018-04-19 05:21:11 -0400

    Updated pseudocode outline, using a switch statement instead of while loop

--- CL-Calculator/CL-Calculator/Program.cs
+++ CL-Calculator/CL-Calculator/Program.cs
@@ -10,15 +10,22 @@ namespace CL_Calculator
     {
         static void Main(string[] args)
         {
-            // while user does not press 'q'...
-                // ...display menu and accept user input
-                // if user input is 1 call addition function
-                // if user input is 2 call subtraction function
-                // if user input is 3 call multiplication function
-                // if user input is 4 call division function
-                // if user input is q exit program
+            // Menu: please enter two numbers
+            // Menu: do you want to add subtract multiply or divide?
+            // Menu: Please enter + - * or /
+            // Menu: Please enter q to quit
 
-            // Note to self: do I need seperate functions to call or just use the built in math functions?
+            // switch
+                // case +
+                    // add two numbers and return sum
+                // case -
+                    // subtract 2nd # from 1st and return difference
+                // case * 
+                    // multiply two numbers and return product
+                // case /
+                    // divide 1st # by 2nd and return quotient
+                // case q
+                    // exit program
         }
     }
 }
