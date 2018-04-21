a7a9858ee0807bf4348914de46b1046384b5b87a
Marshall Ferguson <moferg01@louisville.edu>
2018-04-21 09:56:08 -0400

    Added do while loop, program now runs until user chooses to exit

--- CL-Calculator/CL-Calculator/Program.cs
+++ CL-Calculator/CL-Calculator/Program.cs
@@ -10,47 +10,45 @@ namespace CL_Calculator
     {
         static void Main(string[] args)
         {
+            int userChoice;
             Console.WriteLine("Welcome to the Command Line Calculator!");
-            Console.WriteLine("Please enter two numbers to either add, subtract, multiply, or divide.");
-            string num1String = Console.ReadLine();
-            float num1 = Convert.ToSingle(num1String);
-            string num2String = Console.ReadLine();
-            float num2 = Convert.ToSingle(num2String);
-           
-            Console.WriteLine("Would you like to add, subtract, multiply, or divide?");
-            Console.WriteLine("Please enter +, -, *, or / for your choice.");
-            string userChoice = Console.ReadLine();
-            // Menu: Please enter q to quit
 
-            switch(userChoice)
-            {
-                case "+":
-                    // add two numbers and return sum
-                    float sum = num1 + num2;
-                    Console.WriteLine($"{num1String} + {num2String} = {sum}");
-                    break;
-                case "-":
-                    // subtract 2nd # from 1st and return difference
-                    float diff = num1 - num2;
-                    Console.WriteLine($"{num1String} - {num2String} = {diff}");
-                    break;
-                case "*":
-                    // multiply two numbers and return product
-                    float product = num1 * num2;
-                    Console.WriteLine($"{num1String} * {num2String} = {product}");
-                    break;
-                case "/":
-                    // divide 1st # by 2nd and return quotient
-                    float quotient = num1 / num2;
-                    Console.WriteLine($"{num1String} / {num2String} = {quotient}");
-                    break;
-                // case q
-                    // exit program
-            }
+            do
+            {          
+                Console.WriteLine("Please enter two numbers to either add, subtract, multiply, or divide.");
+                string num1String = Console.ReadLine();
+                float num1 = Convert.ToSingle(num1String);
+                string num2String = Console.ReadLine();
+                float num2 = Convert.ToSingle(num2String);
 
-            Console.WriteLine("Press any key to exit.");
-            // ReadKey() is needed or console will close before user can see output
-            Console.ReadKey();
+                Console.WriteLine("Would you like to add, subtract, multiply, or divide?");
+                Console.WriteLine("1) Add 2) Subtract 3) Multiply 4) Divide 5) Quit");
+                userChoice = Convert.ToInt32(Console.ReadLine());
+
+                switch (userChoice)
+                {
+                    case 1:
+                        // add two numbers and return sum
+                        float sum = num1 + num2;
+                        Console.WriteLine($"{num1String} + {num2String} = {sum}");
+                        break;
+                    case 2:
+                        // subtract 2nd # from 1st and return difference
+                        float diff = num1 - num2;
+                        Console.WriteLine($"{num1String} - {num2String} = {diff}");
+                        break;
+                    case 3:
+                        // multiply two numbers and return product
+                        float product = num1 * num2;
+                        Console.WriteLine($"{num1String} * {num2String} = {product}");
+                        break;
+                    case 4:
+                        // divide 1st # by 2nd and return quotient
+                        float quotient = num1 / num2;
+                        Console.WriteLine($"{num1String} / {num2String} = {quotient}");
+                        break;
+                }
+            } while (userChoice != 5);
         }
     }
 }
